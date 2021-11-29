d87e9ded12b0321d07b60d64d489973762ece35a
chien.lo <yunchienlo@gmail.com>
2021-11-29 11:31:36 +0800

    [Dev] layout 基礎路由

---
+++ src/components/layout/NuLayout.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="nu-layout">
+    <h1>sdf</h1>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NuLayout",
+};
+</script>
+
+<style></style>
